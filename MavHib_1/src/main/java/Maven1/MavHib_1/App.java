e7d094ee2abd1c703b1c58143fdf19f2d780b4a6
pramit.deb <pramit.deb@IRIS-CSG-1293.irissoftware.com>
2019-08-05 11:51:12 +0530

    new MavHib

--- MavHib_1/src/main/java/Maven1/MavHib_1/App.java
+++ MavHib_1/src/main/java/Maven1/MavHib_1/App.java
@@ -1,5 +1,7 @@
 package Maven1.MavHib_1;
 
+import java.util.List;
+
 import com.dao.HeroDao;
 import com.dao.HeroDaoImpl;
 import com.frame.Hero;
@@ -12,18 +14,54 @@ public class App
 {
     public static void main( String[] args )
     {
-    	Hero h=new Hero();
-    	//p.setId(101);
-    	h.setName("Genji");
-    	h.setAge(35);
+    	//Hero h1=new Hero();
+    	//Hero u=new Hero();
+    	//u.setId(2);
+    	
+    	//u.setName("Hanzo");
+    	//h1.setName("Dva");
+    	//h1.setAge(19);
     	
     	HeroDao daoObj=new HeroDaoImpl();
-    	boolean r=daoObj.addHero(h);
-    	if(r){
-    		System.out.println("Person added");
+    	//boolean h3=daoObj.deleteHero(3);
+    	Hero h2=daoObj.getHero(2);
+    	if(h2==null) {
+    		System.out.println("hero doesnt exist");
     	}
     	else {
-    		System.out.println("Person not added");
+    		h2.setAge(40);
+    		
+    	//System.out.println(h2);
+    	boolean h4=daoObj.updateHero(h2);
+    	if(h4) {
+    		System.out.println("hero updated");
     	}
+    	else {
+    	System.out.println("not updated");	
+    	}
+    	}
+    	//if(h3) {
+    	//	System.out.println("hero deleted");
+    //	}
+    //	else {
+    	//	System.out.println("not deleted");
+    //	}
+    	//boolean r=daoObj.addHero(h1);
+    	//if(r){
+    	//	System.out.println("Person added");
+    	//}
+    	//else {
+    	//	System.out.println("Person not added");
+    	//}
+    	List<Hero> hero=daoObj.getAllHeroes();
+    	//System.out.println(hero);
+    	//for(int i=0;i<hero.size();i++) {
+    	//	System.out.println(hero.get(i).getId());
+    	//	System.out.println(hero.get(i).getName());
+    	//	System.out.println(hero.get(i).getAge());
+    	//for(Hero h:hero) {
+    	//	System.out.println(h);
+    	//}
+    	hero.forEach(System.out::println);
     }
 }
--- MavHib_1/src/main/java/com/dao/HeroDaoImpl.java
+++ MavHib_1/src/main/java/com/dao/HeroDaoImpl.java
@@ -7,7 +7,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.frame.Hero;
-import com.portal.entities.Employee;
+
 import com.providers.SessionFactoryProvider;
 
 public class HeroDaoImpl implements HeroDao {
@@ -23,11 +23,11 @@ public class HeroDaoImpl implements HeroDao {
 		return true;
 	}
 
-	public boolean deleteHero(int personId) {
+	public boolean deleteHero(int hId) {
 		Session session=SessionFactoryProvider.getSF().openSession();
 		Transaction tx=session.beginTransaction();
 		
-		Hero p=session.get(Hero.class, personId);
+		Hero p=session.get(Hero.class, hId);
 		if(p==null){
 			return false;
 		}
@@ -39,11 +39,11 @@ public class HeroDaoImpl implements HeroDao {
 		return true;
 	}
 
-	public boolean updateHero(Hero p) {
+	public boolean updateHero(Hero h) {
 		Session session=SessionFactoryProvider.getSF().openSession();
 		Transaction tx=session.beginTransaction();
 		
-		session.update(p);
+		session.update(h);
 		tx.commit();
 		session.close();
 		
@@ -51,9 +51,9 @@ public class HeroDaoImpl implements HeroDao {
 		return true;
 	}
 
-	public Hero getHero(int pId) {
+	public Hero getHero(int hId) {
 		Session session=SessionFactoryProvider.getSF().openSession();
-		Hero p=session.get(Hero.class,pId);
+		Hero p=session.get(Hero.class,hId);
 		session.close();
 		
 		return p;
@@ -62,7 +62,10 @@ public class HeroDaoImpl implements HeroDao {
 	public List<Hero> getAllHeroes() {
 		List<Hero> elist=new ArrayList<>();
 		Session session=SessionFactoryProvider.getSF().openSession();
-		Hero p=session.getClass()<Hero> ;
+		 elist = (List<Hero>)session.createQuery("FROM Hero",Hero.class).list();
+		 if(elist.size()>0) {
+			 return elist;
+		 }
 		return null;
 	}
 
