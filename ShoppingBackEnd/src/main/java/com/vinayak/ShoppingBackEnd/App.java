2e3604e03309b3658128de974d22284a2fcb1542
Pramit <Pramit@LAPTOP-LSM5JSDS>
2019-08-12 20:06:55 +0530

    backend w/ category and product

---
+++ ShoppingBackEnd/src/main/java/com/vinayak/ShoppingBackEnd/App.java
@@ -0,0 +1,59 @@
+package com.vinayak.ShoppingBackEnd;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.vinayak.Dao.CategoryDao;
+import com.vinayak.Dao.ProductDao;
+import com.vinayak.model.Category;
+import com.vinayak.model.Product;
+
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+    	System.out.println("enter category or product");
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+    	 String name = reader.readLine();
+    	 if(name.equals("category")) {
+    	Category c=new Category();
+    	System.out.println("enter add or view");
+    	c.setCategoryId("1");
+    	c.setCategoryName("Mens Wear");
+    	c.setDescription("Formal and Casual Wear");
+    	
+    	Product p=new Product();
+    	p.setProductId("P1");
+    	p.setProductName("TShirt");
+    	p.setProdesc("Formal and Casual Tees");
+    	
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        
+        CategoryDao obj=context.getBean("categoryDao",CategoryDao.class);
+        ProductDao objp=context.getBean("productDao",ProductDao.class);
+        
+        obj.addCategory(c);
+        System.out.println("Category Added");
+        
+        objp.addProduct(p);
+        System.out.println("Product Added");
+        
+        List<Category> categories=obj.getAllCategories();
+        for(Category cObj:categories) {
+        	System.out.println(cObj.getCategoryId()+" "+cObj.getCategoryName());
+        }
+        
+        List<Product> prods=objp.getAllProducts();
+        for(Product pObj:prods) {
+        	System.out.println(pObj.getProductId()+" "+pObj.getProductName());
+        }
+        
+        
+    }
+}
--- ShoppingBackEnd/src/test/java/com/vinayak/ShoppingBackEnd/AppTest.java
+++ ShoppingBackEnd/src/test/java/com/vinayak/ShoppingBackEnd/AppTest.java
@@ -1,4 +1,4 @@
-package com.springwebdbf1.SpringFinal_frontend;
+package com.vinayak.ShoppingBackEnd;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
